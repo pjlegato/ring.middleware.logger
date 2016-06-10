70b3899d970a93a93e2cb9d88e3f8d8925bb5e20
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-06-09 17:11:42 -0700

    Change org.clojure/clojure to "provided"
    This way it won't be imported as transitive dependency anymore.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ring.middleware.logger "0.5.1-SNAPSHOT"
   :description "Ring middleware to log each request."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.3.0" :scope "provided"]
                  [org.clojars.pjlegato/clansi "1.3.0"]
                  [org.tobereplaced/mapply "1.0.0"]
                  [onelog "0.4.5"]
